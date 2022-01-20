82c0ac00b1dc484ef79e678d4ba90e8084c29ebd
ezzio <thangduong12322@gmail.com>
2022-01-20 19:23:01 +0700

    update db

--- src/db/functionForDB/upload.ts
+++ src/db/functionForDB/upload.ts
@@ -1,7 +1,7 @@
 import { GridFsStorage } from "multer-gridfs-storage";
 import { uuid } from 'uuidv4';
 export const storage = new GridFsStorage({
-  url: process.env.MONGODB_URL || "mongodb+srv://ezzio:123456thang@cluster0.zbyuh.mongodb.net/Horenso_Manager?retryWrites=true&w=majority",
+  url: process.env.MONGODB_URL || "mongodb://localhost:27017/Horenso_Manager",
   options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
     const match = ["image/png", "image/jpeg"];

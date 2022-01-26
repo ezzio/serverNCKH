99697561c528eb02f24557c5562210c7b07deafd
ezzio <thangduong12322@gmail.com>
2022-01-26 16:08:16 +0700

    update index project

--- src/router/Router/projectRouter/index.ts
+++ src/router/Router/projectRouter/index.ts
@@ -7,6 +7,9 @@ import {
   editRoleForUser,
   deleteMemberInProject,
   listAllAttachmentInProject,
+  listInfoProjectForOwner,
+  transferOwnerShipProject,
+  renameProject,
 } from "./projectModal";
 const router = express.Router();
 
@@ -17,4 +20,7 @@ router.post("/create_a_project", createAProject);
 router.post("/editRoleOfUser", editRoleForUser);
 router.post("/deleteUserInProject", deleteMemberInProject);
 router.post("/listAllAttachmentInProject", listAllAttachmentInProject);
+router.post("/listSettingPage", listInfoProjectForOwner);
+router.post("/renameProject", renameProject);
+router.post("/transferProject", transferOwnerShipProject);
 export default router;

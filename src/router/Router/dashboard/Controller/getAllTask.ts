f452197012f1957a6249c6414f7d1b57f9aa6c98
pumk <ddkhoa1206@gmail.com>
2022-02-08 16:35:45 +0700

    fix: change trype

--- src/router/Router/dashboard/Controller/getAllTask.ts
+++ src/router/Router/dashboard/Controller/getAllTask.ts
@@ -2,6 +2,7 @@ import { Job_Schema } from './../../../../db/schema/jobs_Schema';
 import task from './../../../../db/schema/task_Schema';
 
 import { Request, Response } from 'express';
+import { ObjectId } from 'mongoose';
 
 export async function getAllTask(req: Request, res: Response) {
   let body = req.body;
@@ -9,7 +10,7 @@ export async function getAllTask(req: Request, res: Response) {
   let jobsInProject = await Job_Schema.find({ projectowner: body.idProject })
     .lean()
     .exec();
-  let idJobs: Array<String> = jobsInProject.map((job) => job._id);
+  let idJobs: Array<ObjectId> = jobsInProject.map((job) => job._id);
   for (let id of idJobs) {
     let temp = await task.find({ idJobOwner: id });
     result.push(...temp);

a38135389599de477b341e4ff3dfbeef8b49b21b
pumk <ddkhoa1206@gmail.com>
2022-02-09 10:49:11 +0700

    feat: sign up

--- src/db/schema/User_Schema.ts
+++ src/db/schema/User_Schema.ts
@@ -1,22 +1,20 @@
-import { model, Schema, Model } from "mongoose";
-import userSchemaInterface from "../interface/userSchemaInterface";
+import { model, Schema, Model } from 'mongoose';
+import userSchemaInterface from '../interface/userSchemaInterface';
 const User = new Schema(
   {
-
-    user_name: { type: String, required: true  , unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     avatar: { type: String },
     display_name: { type: String },
     bio: { type: String },
+    phone: { type: String },
     company: { type: String },
-    location: { type: String },
-    email: { type: String },
-    githubToken: { type: String },
+    address: { type: String },
     createAt: { type: Date, default: Date.now },
-    InfoAllProjectJoin: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+    InfoAllProjectJoin: [{ type: Schema.Types.ObjectId, ref: 'Project' }],
   },
-  { collection: "User" }
+  { collection: 'User' }
 );
 
 // const DonHang = ;
-export default model<userSchemaInterface>("user_Schema", User);
+export default model<userSchemaInterface>('user_Schema', User);

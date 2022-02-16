db1b111624eb78d77c51d9b768e7fa8642a916bd
ezzio <thangduong12322@gmail.com>
2022-02-16 10:42:48 +0700

    llist conversation In project

---
+++ src/db/functionForDB/getInFoUserInArray.ts
@@ -0,0 +1,22 @@
+import User_Schema from "../../db/schema/User_Schema";
+import {
+  model,
+  Schema,
+  Model,
+  Document,
+  ObjectId,
+  LeanDocument,
+} from "mongoose";
+
+export default async (listInMemberInRoom: LeanDocument<ObjectId>[]) => {
+  let infOListMember: any[] = [];
+  for (const eachUser of listInMemberInRoom) {
+    let infoUser = await User_Schema.find({ _id: eachUser }).lean().exec();
+    infOListMember.push({
+      id: infoUser[0]._id,
+      display_name: infoUser[0].display_name,
+      avatar: infoUser[0].avatar,
+    });
+  }
+  return infOListMember;
+};
--- src/router/Router/conversationRouter/conversationModal.ts
+++ src/router/Router/conversationRouter/conversationModal.ts
@@ -2,6 +2,7 @@ import roomConversation_Schema from "../../../db/schema/roomConversation_Schema"
 import { Request, Response } from "express";
 import conversation_Schema from "../../../db/schema/conversation_Schema";
 import Project_Schema from "../../../db/schema/Project_Schema";
+import listMemberInRoom from "../../../db/functionForDB/getInFoUserInArray";
 
 export const createARoomInConversation = async (
   req: Request,
@@ -33,6 +34,7 @@ export const listConversationInProject = async (
   res: Response
 ) => {
   let { idProject } = req.body;
+  let result: any[] = [];
   let findProject = await conversation_Schema
     .find({ projectOwner: idProject })
     .lean()
@@ -40,20 +42,23 @@ export const listConversationInProject = async (
 
   let Listchannel = findProject[0].Listchannel;
   for (const eachChannal of Listchannel) {
-    let eachChannalInfo = {
-      roomName: eachChannal.roomName,
-      roomConversation: [],
-    };
+    let roomConversation: any[] = [];
     if (eachChannal.roomConversation.length > 0) {
       for (const eachConersation of eachChannal.roomConversation) {
         let room = await roomConversation_Schema
           .find({ _id: eachConersation })
           .lean()
           .exec();
-        console.log(room);
+        roomConversation.push({
+          name: room[0].name,
+          textChat: room[0].textChat,
+          memberInRoom: listMemberInRoom(room[0].memberInRoom),
+        });
+
       }
     }
+    result.push({ roomName: eachChannal.roomName, roomConversation });
   }
   // console.log(Listchannel);
-  res.send({ isSuccess: true });
+  res.send({ isSuccess: true , result });
 };
